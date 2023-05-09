7b8b5015624fddc8c5c7cc4c2c6dd3723a3ee589
Morgan McCauley <morgan@mccauley.co.nz>
2023-05-10 10:22:12 +1200

    refactor: Use kitty module in telescope

--- lua/telescope/_extensions/kitty.lua
+++ lua/telescope/_extensions/kitty.lua
@@ -7,6 +7,7 @@ local conf = require('telescope.config').values
 local utils = require('telescope.utils')
 
 local config = require('telescope._extensions.config')
+local kitty = require('kitty')
 
 local list_projects = function(workspaces)
   local projects = {}
@@ -55,13 +56,7 @@ local kitty_projects = function(opts)
         actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
 
-        local raw_results = utils.get_os_command_output({
-          'kitty',
-          '@',
-          '--to=unix:/tmp/kitty-' .. vim.env.KITTY_PID,
-          'ls'
-        })
-        local kitty_windows = vim.json.decode(table.concat(raw_results))
+        local kitty_windows = kitty.list_windows()
 
         local tab_exists = false
         for _, tab in ipairs(kitty_windows[1].tabs) do
@@ -72,30 +67,17 @@ local kitty_projects = function(opts)
         end
 
         if tab_exists then
-          vim.loop.spawn('kitty', {
-            args = {
-              '@',
-              '--to=unix:/tmp/kitty-' .. vim.env.KITTY_PID,
-              'focus-tab',
-              '--match=title:' .. selection.value.basename,
-            }
-          })
-          return true
+          kitty.focus_tab({ title = selection.value.basename })
         else
-          vim.loop.spawn('kitty', {
-            args = {
-              '@',
-              '--to=unix:/tmp/kitty-' .. vim.env.KITTY_PID,
-              'launch',
-              '--type=tab',
-              '--tab-title=' .. selection.value.basename,
-              '--window-title=' .. selection.value.basename,
-              '--cwd=' .. selection.value.absolute_path,
-              config.command
-            }
+          kitty.launch_tab({
+            tab_title = selection.value.basename,
+            window_title = selection.value.basename,
+            cwd = selection.value.absolute_path,
+            -- cmd = config.command
           })
         end
       end)
+
       return true
     end,
   }):find()
