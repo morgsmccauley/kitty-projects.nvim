d911b9474941abc80b8d8ca5fb6f300ca1fc1ce9
Morgan Mccauley <morgan@mccauley.co.nz>
2023-07-05 10:40:29 +1200

    refactor: Extract list logic to scoped functions

--- lua/kitty/projects.lua
+++ lua/kitty/projects.lua
@@ -8,7 +8,37 @@ local state = require('kitty.state')
 
 local M = {}
 
-function M.list()
+local function list_sub_directories(parent_dir)
+  return Job:new({
+    command = 'find',
+    args = {
+      parent_dir,
+      '-maxdepth', '1',
+      '-mindepth', '1',
+      '-type', 'd'
+    },
+  }):sync()
+end
+
+local function list_project_paths()
+  local paths = {}
+
+  for _, path in ipairs(config.options.project_paths) do
+    if type(path) == 'table' then
+      local dir = path[1]
+
+      local sub_directories = list_sub_directories(dir)
+
+      utils.merge_tables(paths, sub_directories)
+    else
+      table.insert(paths, path)
+    end
+  end
+
+  return paths
+end
+
+local function merge_kitty_windows(project_paths)
   local all_windows = commands.list_windows()
   local windows = all_windows[1].tabs[1].windows
 
@@ -19,73 +49,51 @@ function M.list()
   local open_projects = {}
   local unopen_projects = {}
 
-  for _, project_path in ipairs(config.options.project_paths) do
-    local paths = {}
+  for _, path in ipairs(project_paths) do
+    local basename = vim.fn.fnamemodify(path, ':t')
 
-    if type(project_path) == 'table' then
-      local dir = project_path[1]
+    local active_window = utils.find_table_entry(windows, function(entry)
+      return entry.title == basename
+    end)
 
-      paths = Job:new({
-        command = 'find',
-        args = {
-          dir,
-          '-maxdepth', '1',
-          '-mindepth', '1',
-          '-type', 'd'
-        },
-      }):sync()
-    else
-      paths = {
-        project_path
-      }
-    end
-
-    for _, path in ipairs(paths) do
-      local basename = vim.fn.fnamemodify(path, ':t')
-
-      local active_window = utils.find_table_entry(windows, function(entry)
-        return entry.title == basename
-      end)
-
-      if active_window then
-        if active_window.is_focused then
-          current_project = Project:new({
-            id = active_window.id,
-            name = basename,
-            path = path,
-            is_focused = true,
-            was_focused = false,
-            open = true
-          })
-        elseif previous_project_name == basename then
-          previous_project = Project:new({
-            id = active_window.id,
-            name = basename,
-            path = path,
-            is_focused = false,
-            was_focused = true,
-            open = true
-          })
-        else
-          table.insert(open_projects, Project:new({
-            id = active_window.id,
-            name = basename,
-            path = path,
-            is_focused = false,
-            was_focused = false,
-            open = true
-          }))
-        end
+    if active_window then
+      if active_window.is_focused then
+        current_project = Project:new({
+          id = active_window.id,
+          name = basename,
+          path = path,
+          is_focused = true,
+          was_focused = false,
+          open = true
+        })
+      elseif previous_project_name == basename then
+        previous_project = Project:new({
+          id = active_window.id,
+          name = basename,
+          path = path,
+          is_focused = false,
+          was_focused = true,
+          open = true
+        })
       else
-        table.insert(unopen_projects, Project:new({
-          id = nil,
+        table.insert(open_projects, Project:new({
+          id = active_window.id,
           name = basename,
           path = path,
           is_focused = false,
           was_focused = false,
-          open = false
+          open = true
         }))
       end
+    else
+      table.insert(unopen_projects, Project:new({
+        id = nil,
+        name = basename,
+        path = path,
+        is_focused = false,
+        was_focused = false,
+        open = false
+      }))
     end
   end
 
@@ -104,6 +112,13 @@ function M.list()
   end
 
   return utils.merge_tables(all_projects, unopen_projects)
+end
+
+function M.list()
+  local project_paths = list_project_paths()
+  local kitty_projects = merge_kitty_windows(project_paths)
+
+  return kitty_projects
 end
 
 function M.close(project)
