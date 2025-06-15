b68678d685a62c5ca052af79229a75a82499f09b
Morgan Mccauley <morgan@mccauley.co.nz>
2025-06-15 21:05:15 +1200

    feat: Add user commands and enhanced API
    - Add :KittyProjects, :KittyProjectsRefresh, :KittyProjectsCurrent commands
    - Expose additional API functions (refresh, get_current_project, list)
    - Improve user experience with convenient command access
    - Better integration with Neovim's command system

--- lua/kitty/init.lua
+++ lua/kitty/init.lua
@@ -6,6 +6,25 @@ local M = {}
 function M.setup(opts)
   config.setup(opts)
   sessions.setup()
+  
+  -- Create user commands
+  vim.api.nvim_create_user_command('KittyProjects', function()
+    M.projects()
+  end, { desc = 'Open kitty projects picker' })
+  
+  vim.api.nvim_create_user_command('KittyProjectsRefresh', function()
+    require('kitty.projects').refresh()
+    vim.notify('kitty-projects: Cache refreshed')
+  end, { desc = 'Refresh project cache' })
+  
+  vim.api.nvim_create_user_command('KittyProjectsCurrent', function()
+    local current = require('kitty.projects').get_current_project()
+    if current then
+      vim.notify('Current project: ' .. current.name .. ' (' .. current.path .. ')')
+    else
+      vim.notify('No current project found')
+    end
+  end, { desc = 'Show current project info' })
 end
 
 function M.projects()
@@ -14,6 +33,19 @@ function M.projects()
   else
     vim.cmd('Telescope kitty projects')
   end
+end
+
+-- Expose additional API functions
+M.refresh = function()
+  return require('kitty.projects').refresh()
+end
+
+M.get_current_project = function()
+  return require('kitty.projects').get_current_project()
+end
+
+M.list = function()
+  return require('kitty.projects').list()
 end
 
 return M
